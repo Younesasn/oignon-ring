0e5ed39d287e856b14160bf6deff5bab8c17e3f4
ASSANI Younes <younesassani@gmail.com>
2025-06-11 01:05:45 +0200

    inisialisation du jeu

--- src/main/java/fr/younesasn/App.java
+++ src/main/java/fr/younesasn/App.java
@@ -1,13 +1,13 @@
 package fr.younesasn;
 
+import fr.younesasn.services.CombatManager;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        CombatManager.initialisationPartie();
     }
 }
